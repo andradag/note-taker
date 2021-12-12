a4ac42f0a8c535d79572edcb414e2ba4cfa19ee5
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-12-12 20:54:34 +0000

    getting view routes connetcted

--- src/controllers/view/index.js
+++ src/controllers/view/index.js
@@ -1,5 +1,14 @@
-const renderNotesPage = (req, res) => {};
-const renderHomePage = (req, res) => {};
+const path = require("path");
+
+const renderNotesPage = (req, res) => {
+  const filePath = path.join(__dirname, "../../../public/notes.html");
+  res.sendFile(filePath);
+};
+
+const renderHomePage = (req, res) => {
+  const filePath = path.join(__dirname, "../../../public/index.html");
+  res.sendFile(filePath);
+};
 
 module.exports = {
   renderNotesPage,
